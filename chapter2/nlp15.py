9c46594bb0516e2b3e217eed54447e00d39d59ec
mitaketsu <mtkmpool@gmail.com>
2020-07-02 00:08:47 +0900

    nlp11-15

---
+++ chapter2/nlp15.py
@@ -0,0 +1,12 @@
+import sys
+
+def readline(file_name):
+    with open(file_name, "r") as target_file:
+        return target_file.readlines()
+
+file_name = sys.argv[1]
+N = sys.argv[2]
+
+target_lines = readline(file_name)
+
+print("".join(target_lines[-1 * int(N):]))
